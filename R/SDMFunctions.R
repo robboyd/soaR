48414b28931c020c96c1c4b5a3263ed08700e7c7
Rob Boyd <robboy@ceh.ac.uk>
2020-12-02 19:38:34 +0000

    Add function to optimise threshold probabilities of occurrence

--- R/SDMFunctions.R
+++ R/SDMFunctions.R
@@ -518,3 +518,113 @@ stackPreds <- function(inPath, group, write = TRUE, outPath) {
   }
 
 }
+
+
+
+#' Create binary presence/ absence predictions from probabilities of occurrence.
+#'
+#' This function optimises a threshold probability above which a grid cell is classed as a presence,
+#' and below which it is classed as an absence. Optimisation is achieved
+#' by maximising the proportion of data correctly classified.
+#'
+#' @param inPath String. Path to the ensemble model outputs.
+#' @param group string. Taxonomic group. There must be a file in inPath.
+#' @param species String. There must be a file in inPath called /<group>/<species>.asc.
+#' @param outPath String. Where to store the outputs.
+#' @param map Logical. If true writes an ascii file to outPath.
+#' @param presAb String. Presence/ pseudo absence data created by createPresAb for the chosen group.
+#' @export
+#' @return
+#' A dataframe with four columns: Threshold probability of occurrence, species and group.
+#' Optionally a raster can be written to file.
+
+optOccThresh <- function(inPath, group, species, outPath, map, presAb) {
+
+  print(species)
+
+  if (!dir.exists(outPath)) {
+
+    dir.create(outPath)
+
+  }
+
+  file <- paste0(inPath, group, "/", species, "_ensemble.asc")
+
+  if (file.exists(file)) {
+
+    out <- raster::raster(file)
+
+    dat <- presAb[[which(names(presAb) == species)]]
+
+    nRecs <- nrow(dat$Presence)
+
+    presProb <- raster::extract(out, dat$Presence)
+
+    abProb <- raster::extract(out, dat$pseudoAbsence)
+
+    obs <- c(rep(1, nrow(dat$Presence)), rep(0, nrow(dat$pseudoAbsence)))
+
+    preds <- c(presProb, abProb)
+
+    DATA <- data.frame(id = 1:length(obs),
+                       obs = obs,
+                       pred = preds)
+
+    thresh <- seq(0,1, length.out = 50)
+
+    opt <- PresenceAbsence::optimal.thresholds(DATA, thresh, opt.methods = 3, na.rm = T)
+
+    opt <- as.numeric(opt[2])
+
+    auc <- PresenceAbsence::auc(DATA, st.dev = F, na.rm = T)
+
+    confMat <- PresenceAbsence::cmx(DATA, threshold = opt, na.rm = T)
+
+    sens <- PresenceAbsence::sensitivity(confMat, st.dev = F)
+
+    spec <- PresenceAbsence::specificity(confMat, st.dev = F)
+
+    tss <- (sens + spec) - 1
+
+    kappa <- PresenceAbsence::Kappa(confMat, st.dev = F)
+
+    sumStats <- data.frame(group = group,
+                           species = species,
+                           nRecs = nRecs,
+                           thresh = opt,
+                           auc = auc,
+                           sensitivity = sens,
+                           specificity = spec,
+                           tss = tss,
+                           kappa = kappa)
+
+    if (map == TRUE) {
+
+      binPred <- out
+
+      binPred[binPred < opt] <- 0
+
+      binPred <- as.logical(binPred)
+
+      writeRaster(binPred,
+                  paste0(outPath, species),
+                  format = "ascii",
+                  overwrite = T)
+
+      par(mfrow = c(1,2), mar = c(2,2,2,2))
+
+      plot(out, col = matlab.like(50), axes = F)
+
+      plot(binPred, axes = F, legend = F)
+
+    }
+
+  } else {
+
+    sumStats <- NULL
+
+  }
+
+  return(sumStats)
+
+}
