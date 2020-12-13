e86603b1cfb7b36babc3bde352f829c8dc5d1e79
Rob Boyd <xw864954@live.reading.ac.uk>
2020-12-13 16:28:28 +0000

    Include priority species names as dataset

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Name of species one or more of England, Scotland, Wales or Northern Ireland's priority lists.
+#'
+#' @source JNCC taxon designations https://hub.jncc.gov.uk/assets/478f7160-967b-4366-acdf-8941fd33850b.
+#'              Also includes Biological Records Centre concept codes for species which have them.
+#' @format A data frame with one column: species name.
+#' @examples
+#' \dontrun{
+#'  prioSpecies
+#' }
+"prioSpecies"
